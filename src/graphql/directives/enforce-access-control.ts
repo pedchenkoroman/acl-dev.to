55bded43182a3f0cd1b14220aa9831be35f2c732
Roman Pedchenko <roman.pedchenko.88@gmail.com>
2025-01-08 21:51:01 +0100

    Feat/create access control directive (#2)
    * feat: create access control directive
    
    * feat: change separator
    
    * feat: remove comment
    
    ---------
    
    Co-authored-by: romanpedchenko <r.pedchenko@mylette.nl>

---
+++ src/graphql/directives/enforce-access-control.ts
@@ -0,0 +1,55 @@
+import { defaultFieldResolver } from 'graphql/execution';
+import { getDirective, MapperKind, mapSchema } from '@graphql-tools/utils';
+
+import type { GraphQLSchema } from 'graphql/type';
+
+/**
+ * GraphQL directive to validate access to request field.
+ */
+const enforceAccessControl = (directiveName: string) => {
+  return {
+    enforceAccessControlTypeDefs: `directive @${directiveName}(namespace: String!) on FIELD_DEFINITION`,
+    enforceAccessControlTransformer: (schema: GraphQLSchema) =>
+      mapSchema(schema, {
+        [MapperKind.OBJECT_FIELD](fieldConfig) {
+          const [directive] = getDirective(schema, fieldConfig, directiveName) || [];
+          if (!directive) return fieldConfig;
+
+          const { resolve = defaultFieldResolver } = fieldConfig;
+
+          const patchedResolve = async (
+            source: any,
+            args: any,
+            context: { action: string },
+            info: any,
+          ) => {
+            const [action] = context.action.split('|').filter((action) => {
+              const namespace = action.split('/').at(0);
+              return namespace === directive.namespace;
+            });
+
+            if (!action) {
+              throw new Error(`Namespace ${directive.namespace} does not exist!`);
+            }
+
+            const [, allowedActions] = action.split('/');
+
+            console.log(allowedActions);
+
+            if (
+              !allowedActions.split(',').some((action) => ['*', info.fieldName].includes(action))
+            ) {
+              throw new Error(`The action ${info.fieldName} is not allowed!`);
+            }
+
+            return resolve(source, args, context, info);
+          };
+
+          return { ...fieldConfig, resolve: patchedResolve };
+        },
+      }),
+  };
+};
+
+export const { enforceAccessControlTypeDefs, enforceAccessControlTransformer } =
+  enforceAccessControl('enforceAccessControl');
